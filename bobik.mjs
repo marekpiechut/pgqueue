fc2af18efb05fcb0b052bf918714b4967fd533d0
Marek Piechut <marek@dayone.pl>
2023-10-05 11:19:36 +0200

    Subscribe/push plsql functions with random node being notified

---
+++ bobik.mjs
@@ -0,0 +1,47 @@
+import pg from 'pg'
+import chalk from 'chalk'
+
+const colors = ['red', 'green', 'blue', 'magenta', 'cyan', 'gray']
+const events = ['bobik', 'mobik', 'zigi', 'haha']
+
+const listen = async amount => {
+	console.log(chalk.green(`Starting ${amount} listeners`))
+	const promises = []
+	for (let i = 0; i < amount; i++) {
+		const color = colors[i % colors.length]
+		const conn = new pg.Client({ user: 'postgres' })
+		conn.on('notification', msg => {
+			console.log(chalk[color](`${i}: `), msg.payload)
+		})
+
+		promises.push(
+			conn
+				.connect()
+				.then(() =>
+					conn.query(`select pgqueues.subscribe($1, $2)`, ['test' + i, events])
+				)
+		)
+	}
+	await Promise.all(promises)
+}
+
+const emit = async delay => {
+	console.log(chalk.green(`Publishing event every ${delay} ms`))
+	const conn = new pg.Client({ user: 'postgres' })
+	let i = 0
+	conn.connect()
+	setInterval(() => {
+		conn.query(`select pgqueues.push($1, NULL, NULL, NULL)`, [
+			events[i % events.length],
+		])
+		i++
+	}, delay)
+}
+
+if (process.argv[2] === 'listen') {
+	listen(parseInt(process.argv[3]))
+} else if (process.argv[2] === 'emit') {
+	emit(parseInt(process.argv[3]))
+} else {
+	console.log('Usage: node bobik.mjs <listen|emit> <amount|delay>')
+}
--- packages/jobs/src/schema/versions/v1.sql.ts
+++ packages/jobs/src/schema/versions/v1.sql.ts
@@ -8,6 +8,14 @@ type Config = {
 export default (config: Config): Evolution => ({
 	ups: [
 		`CREATE SCHEMA IF NOT EXISTS ${config.schema}`,
+		`CREATE TYPE ${config.schema}.JOB_STATE AS ENUM ('PENDING', 'RUNNING', 'COMPLETED', 'FAILED')`,
+		// -- TABLES -- //
+		`CREATE TABLE ${config.schema}.LISTENERS (
+			nodeId VARCHAR(64) NOT NULL,
+			type VARCHAR(64) NOT NULL,
+			pid INTEGER NOT NULL,
+			PRIMARY KEY(pid, type)
+		)`,
 		`CREATE TABLE ${config.schema}.QUEUE (
 			id UUID NOT NULL,
 			key VARCHAR(64) UNIQUE DEFAULT NULL,
@@ -19,7 +27,7 @@ export default (config: Config): Evolution => ({
 			created TIMESTAMP NOT NULL,
 			updated TIMESTAMP,
 			started TIMESTAMP,
-			state VARCHAR(16) NOT NULL,
+			state ${config.schema}.JOB_STATE NOT NULL,
 			run_after TIMESTAMP,
 			priority INTEGER,
 			payload JSONB,
@@ -66,17 +74,93 @@ export default (config: Config): Evolution => ({
 			payload JSONB,
 			PRIMARY KEY(id)
 		)`,
+		// -- SUBSCRIBE FUNCTIONS -- //
+		`CREATE OR REPLACE FUNCTION
+			${config.schema}.SUBSCRIBE(nodeId VARCHAR, types VARCHAR[]) RETURNS INTEGER AS $$
+				BEGIN
 
-		`CREATE OR REPLACE FUNCTION ${config.schema}.QUEUE_ADDED() RETURNS trigger AS $$
+				INSERT INTO ${config.schema}.LISTENERS (nodeId, type, pid)
+				SELECT nodeId, type, pg_backend_pid()
+				FROM unnest(types) AS type
+				ON CONFLICT DO NOTHING;
+
+				EXECUTE 'LISTEN ' || quote_ident(format('pgqueue:listener:%s:%s', nodeId, pg_backend_pid()));
+
+				RETURN cardinality(types);
+				END;
+			$$ LANGUAGE plpgsql;
+		`,
+		`CREATE OR REPLACE FUNCTION
+		${config.schema}.UNSUBSCRIBE(id VARCHAR, types VARCHAR[]) RETURNS BOOLEAN AS $$
 			BEGIN
-				PERFORM pg_notify('pgqueue:queue:added', NEW.type);
+
+			IF cardinality(types) = 0 OR types IS NULL THEN
+				DELETE FROM ${config.schema}.LISTENERS WHERE nodeId = id OR pid = pg_backend_pid();
+			ELSE
+				DELETE FROM ${config.schema}.LISTENERS WHERE
+				(nodeId = id OR pid = pg_backend_pid()) AND type = ANY(types);
+			END IF;
+
+			EXECUTE 'UNLISTEN ' || quote_ident(format('pgqueue:listener:%s:%s', nodeId, pg_backend_pid()));
+
+			RETURN FOUND;
+			END;
+		$$ LANGUAGE plpgsql;
+		`,
+		`CREATE OR REPLACE PROCEDURE ${config.schema}.CLEAR_ORPHANED_LISTENERS() AS $$
+			DELETE FROM ${config.schema}.LISTENERS WHERE pid NOT IN ( SELECT pid FROM pg_stat_activity);			
+		$$ LANGUAGE sql;`,
+		// -- QUEUE FUNCTIONS -- //
+		`CREATE OR REPLACE FUNCTION
+			${config.schema}.PUSH(type VARCHAR, payload JSON, priority INTEGER, key VARCHAR)
+			RETURNS ${config.schema}.QUEUE AS $$
+			DECLARE
+				row ${config.schema}.QUEUE%rowtype;
+			BEGIN
+
+			INSERT INTO ${config.schema}.QUEUE
+				(id, type, created, priority, key, payload, state)
+			VALUES
+				(gen_random_uuid(), type, now(), priority, key, payload, 'PENDING')
+			ON CONFLICT ON CONSTRAINT QUEUE_key_key DO UPDATE SET
+				updated		=	now(),
+				priority	=	EXCLUDED.priority,
+				payload		=	EXCLUDED.payload,
+				version		=	QUEUE.version + 1
+			RETURNING * INTO row;
+
+			RETURN row;
+			END;
+		$$ LANGUAGE plpgsql;`,
+		// -- TRIGGERS -- //
+		`CREATE OR REPLACE FUNCTION ${config.schema}.QUEUE_ADDED() RETURNS trigger AS $$
+			DECLARE
+				cursor REFCURSOR;
+				items INTEGER;
+				row ${config.schema}.LISTENERS;
+			BEGIN
+				CALL ${config.schema}.CLEAR_ORPHANED_LISTENERS();
+				OPEN cursor FOR SELECT * FROM ${config.schema}.LISTENERS WHERE type = NEW.type;
+
+				MOVE FORWARD ALL FROM cursor;
+				GET DIAGNOSTICS items = ROW_COUNT;
+				raise notice 'Items: %', items;
+
+				IF items > 0 THEN
+					items := floor(random() * items);
+					MOVE ABSOLUTE (items) FROM cursor; 
+					FETCH cursor INTO row;
+					EXECUTE 'NOTIFY ' || quote_ident(format('pgqueue:listener:%s:%s', row.nodeId, row.pid)) || ', ' || quote_literal(NEW.type);
+				END IF;
+				
+				CLOSE cursor;
 				RETURN NEW;
 			END;
 			$$ LANGUAGE plpgsql;
 		`,
-		`CREATE OR REPLACE TRIGGER TR_SCHEDULE_ADDED
-			AFTER INSERT ON ${config.schema}.QUEUE
-			FOR EACH STATEMENT EXECUTE PROCEDURE
+		`CREATE OR REPLACE TRIGGER TR_QUEUE_ADDED
+			AFTER INSERT OR UPDATE ON ${config.schema}.QUEUE
+			FOR EACH ROW EXECUTE PROCEDURE
 			${config.schema}.QUEUE_ADDED();
 		`,
 		`CREATE OR REPLACE FUNCTION ${config.schema}.SCHEDULE_ADDED() RETURNS trigger AS $$
@@ -88,16 +172,21 @@ export default (config: Config): Evolution => ({
 		`,
 		`CREATE OR REPLACE TRIGGER TR_SCHEDULE_ADDED
 			AFTER INSERT ON ${config.schema}.SCHEDULE
-			FOR EACH STATEMENT EXECUTE PROCEDURE
+			FOR EACH ROW EXECUTE PROCEDURE
 			${config.schema}.SCHEDULE_ADDED();
 		`,
 	],
 	downs: [
+		`DROP FUNCTION ${config.schema}.PUSH`,
+		`DROP FUNCTION ${config.schema}.SUBSCRIBE`,
+		`DROP FUNCTION ${config.schema}.UNSUBSCRIBE`,
+		`DROP TABLE ${config.schema}.LISTENERS`,
 		`DROP TABLE ${config.schema}.QUEUE`,
 		`DROP TABLE ${config.schema}.QUEUE_HISTORY`,
 		`DROP FUNCTION ${config.schema}.QUEUE_ADDED`,
 		`DROP TABLE ${config.schema}.SCHEDULE`,
 		`DROP TABLE ${config.schema}.SCHEDULE_RUNS`,
 		`DROP FUNCTION ${config.schema}.SCHEDULE_ADDED`,
+		`DROP TYPE ${config.schema}.JOB_STATE`,
 	],
 })
